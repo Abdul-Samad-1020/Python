22323dbbbda1b31ef3a6e88e8f9107a08dd05dcb
Abdul Samad <abdulsamadmughal102@gmail.com>
2025-02-14 16:36:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
-const path = "./data.json";
-const data = moment().subtract(5,'d').format();
+import random from "random";
 
-const data = {
-    date: data,
+const path = "./data.json";
+
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
 };
-jsonfile.writeFile(path, data, ()   => {
-    simpleGit().add([path]).commit(date,['--date':date]);
-} );
+
+const makeCommits = (n) => {
+  if(n===0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+  });
+};
+
+makeCommits(100);
